4c113f9b63e2648079b74f898a9c71595e6608d1
Samatar Abdullahi <abdu0675@algonquinlive.com>
2026-04-03 17:36:39 -0400

    modified  file

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
 console.log("APP IS STARTING...");
 const express = require("express");
 const path = require("path");
 
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
 // =====================
-// MIDDLEWARE
+// User Model
 // =====================
+const userSchema = new mongoose.Schema({
+    email: String,
+    username: String,
+    password: String
+});
 
-// Static files (CSS, JS, images)
-app.use(express.static(path.join(__dirname, "public")));
+const User = mongoose.model("User", userSchema)
 
-// Parse form data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 // =====================
 // VIEW ENGINE (PUG)
@@ -52,13 +55,44 @@ app.get("/resources", (req, res) => {
     res.render("resources");
 });
 
+// Register
+app.get("/register", async (req, res) => {
+    const { email, username, password} = req.body;
+
+    if (!email || !username || !password) {
+        return res.json({ message: "All Fields Required"});
+    }
+
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+        return res.json({ message: "User already exists" });
+    }
+
+    const newUser = new User({ email, username, password });
+    await newUser.save();
+
+    res.json({ message: "Registered successfully" });
+});
+
 // Login
-app.get("/login", (req, res) => {
-    res.render("login");
+app.get("/login", async (req, res) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        return res.json({ success: false, message: "User not found"});
+    }
+
+    if (user.password !== password) {
+        return res.json({ success: false, message: "Wrong password"});
+    }
+
+    res.json({success: true });
+
 });
 
 // =====================
-// FORM EXAMPLE (for marks)
+// FORM  
 // =====================
 
 app.post("/contact", (req, res) => {
@@ -73,3 +107,18 @@ app.post("/contact", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// =====================
+// Database
+// =====================
+
+const dbURI = process.env.MONGO_URI;
+
+if (!dbURI) {
+    console.error("ERROR: MONGO_URI is not defined in the .env file.");
+    process.exit(1); // Stop the app if there's no DB connection string
+}
+
+mongoose.connect(dbURI)
+    .then(() => console.log("MongoDB connected!"))
+    .catch((err) => console.error("Failed to connect to MongoDB:", err));
